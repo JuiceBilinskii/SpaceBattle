db91b794bc3f7971f36e14d671e900c37f2742f5
JuiceBilinskii <77838296+JuiceBilinskii@users.noreply.github.com>
2021-01-28 17:04:29 +0200

    Add files via upload

--- data/actions_capturer.py
+++ data/actions_capturer.py
@@ -1,9 +1,13 @@
 class ActionsCapturer:
     def __init__(self):
         self.__key_a = False
+        self.__key_left = False
+        self.__key_right = False
 
     def to_default(self):
         self.__key_a = False
+        self.__key_left = False
+        self.__key_right = False
 
     @property
     def key_a(self):
@@ -12,3 +16,19 @@ class ActionsCapturer:
     @key_a.setter
     def key_a(self, value: bool):
         self.__key_a = value
+
+    @property
+    def key_left(self):
+        return self.__key_left
+
+    @key_left.setter
+    def key_left(self, value: bool):
+        self.__key_left = value
+
+    @property
+    def key_right(self):
+        return self.__key_right
+
+    @key_right.setter
+    def key_right(self, value: bool):
+        self.__key_right = value
