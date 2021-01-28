81c8bf074207bdfb8d04afde3ba8ea4bacce3b57
JuiceBilinskii <77838296+JuiceBilinskii@users.noreply.github.com>
2021-01-28 17:07:42 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 import platform
 import sys
 import pygame
-from pygame import K_ESCAPE, K_F4, K_RALT, K_LALT, QUIT, KEYDOWN
+from pygame import K_F4, K_RALT, K_LALT, QUIT, KEYDOWN, K_a, K_LEFT, K_RIGHT
+from data.controller import Controller
+from data.actions_capturer import ActionsCapturer
+from data.constants import FULL_SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 # Initialization
@@ -13,19 +16,19 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'  # center display
 pygame.init()
 clock = pygame.time.Clock()
 
-FULL_SCREEN = False
+# FULL_SCREEN = False
 
 
 if __name__ == '__main__':
     # Screen
     if FULL_SCREEN:
-        SCREEN_WIDTH = round(pygame.display.Info().current_w)
-        SCREEN_HEIGHT = round(pygame.display.Info().current_h)
         SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
     else:
-        SCREEN_WIDTH = round(pygame.display.Info().current_w * 0.75)
-        SCREEN_HEIGHT = round(pygame.display.Info().current_h * 0.75)
         SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Preparation
+    actions_capturer = ActionsCapturer()
+    controller = Controller(SCREEN)
 
     # Main loop
     while True:
@@ -35,8 +38,19 @@ if __name__ == '__main__':
                                                  and (pressed_keys[K_LALT] or pressed_keys[K_RALT])))
             if event.type == QUIT or alt_f4:
                 sys.exit()
-            if event.type == KEYDOWN and event.key == K_ESCAPE:
-                pass
+            if event.type == KEYDOWN:
+                if event.key == K_a:
+                    actions_capturer.key_a = True
+                if event.key == K_LEFT:
+                    actions_capturer.key_left = True
+                if event.key == K_RIGHT:
+                    actions_capturer.key_right = True
+
+        if controller.update_model(actions_capturer) == 'quit':
+            sys.exit()
+        controller.update_view()
+
+        actions_capturer.to_default()
 
         pygame.display.update()
         clock.tick(30)
