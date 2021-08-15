e566c8d676fd108a66d5e03a6a6b8ce93a52c092
bilin <bilinskij2001@gmail.com>
2021-08-15 18:48:12 +0300

    Uploaded old project

--- data/models/imodel.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from data.actions_capturer import ActionsCapturer
-
-
-class IModel(ABC):
-    @abstractmethod
-    def update(self, actions: ActionsCapturer):
-        pass
